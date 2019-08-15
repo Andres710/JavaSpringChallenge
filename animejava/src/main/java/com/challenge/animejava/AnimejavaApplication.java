087e6a889382b37f671b7c95f87a518f2d7e305f
Andres Lopez <andlopez@endava.com>
2019-08-15 09:40:54 -0500

    Started project with Spring Initializr

---
+++ animejava/src/main/java/com/challenge/animejava/AnimejavaApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.animejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimejavaApplication.class, args);
+	}
+
+}
---
+++ animejava/src/test/java/com/challenge/animejava/AnimejavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.challenge.animejava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnimejavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
